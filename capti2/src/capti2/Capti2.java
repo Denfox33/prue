c34e8f0669d38a329c77988ac36210d1897f7f34
Denfox33 <125207894+Denfox33@users.noreply.github.com>
2023-05-12 15:54:17 +0200

    borrar

--- capti2/src/capti2/Capti2.java
+++ capti2/src/capti2/Capti2.java
@@ -16,7 +16,7 @@ public class Capti2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hola mundo");
+        System.out.println("hola ");
     }
     
 }
